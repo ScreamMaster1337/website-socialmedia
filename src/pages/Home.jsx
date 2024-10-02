f1e70b4d5e7113e9856efdf864277d945842bea5
Mateusz Kosowski <screammaster1337.biznes@gmail.com>
2024-10-02 17:59:27 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../lanacode.png';  // Twoje główne logo
+import logo from '../m4code.png';  // Twoje główne logo
 import poweredLogo from '../m4code.png';  // Logo do stopki
 import Particle from '../Particle.js';
 
@@ -11,10 +11,11 @@ class Home extends React.Component {
                 <section className="content">
                     <div className="container">
                         <div className="logoImg">
-                            <img src={logo} alt="LanaCode logo" />
-                            <strong style={{ color: 'white', fontWeight: 'bold', fontSize: '24px' }}>LanaCode</strong>
-                            <a href="https://discord.gg/Bcd4FxkCkg">Discord</a>
-                            <a href="https://www.youtube.com/@Naysoo_">Youtube</a>
+                            <img src={logo} alt="M4CODE logo" />
+                            <strong style={{ color: 'white', fontWeight: 'bold', fontSize: '24px' }}>M4CODE.PL</strong>
+                            <a href="https://discord.com/invite/kEbWN4DktJ">Discord</a>
+                            <a href="https://www.facebook.com/m4codepl/">Facebook</a>
+                            <a href="https://www.youtube.com/channel/UCXK5ojk47wvuUaoQPHrs4Ng">Youtube</a>
                         </div>
                     </div>
                 </section>
@@ -22,7 +23,7 @@ class Home extends React.Component {
                 {/* Stopka */}
                 <footer className="footer">
                     <div className="footer-left">
-                        <span style={{ color: 'white', fontSize: '17px', fontWeight: 'normal' }}>Copyright © 2024 LanaCode. Wszelkie prawa zastrzeżone.</span>
+                        <span style={{ color: 'white', fontSize: '17px', fontWeight: 'normal' }}>Copyright © 2021-2024 M4CODE.PL. Wszelkie prawa zastrzeżone.</span>
                     </div>
                     <div className="footer-right">
                         <strong style={{ color: 'gray', fontWeight: 'bold', fontSize: '15px' }}>Strona wykonana przez:</strong>
